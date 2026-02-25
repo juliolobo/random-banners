d125878bff76fcf6f5648f500cf5d6dd201b054c
Julio Lobo <juliolobo@lobohost.com.br>
2026-02-25 10:14:55 -0300

    Adicinando mais comentarios

--- functions/ads.js
+++ functions/ads.js
@@ -3,11 +3,11 @@ export const ads = [
         id: 1,
         image: "/images/imagem1.jpg",
         url: "https://cliente1.com",
-        weight: 1,                // quanto maior, mais aparece
-        priority: 1,              // 1 = normal, 2 = destaque
-        active: true,
-        startDate: "2026-01-01",
-        endDate: "2026-12-31"
+        weight: 1,                  // quanto maior, mais aparece
+        priority: 1,                // 1 = normal, 2 = destaque
+        active: true,               // se está ativo ou não
+        startDate: "2026-01-01",    // data para a imagem começar a ser exibida
+        endDate: "2026-12-31"       // data para a imagem parar de ser exibida
     },
     {
         id: 2,
